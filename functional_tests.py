1c43611b48a9149c3e8b06f83e8d6b463cc9a6ac
zhang_jianhua <zhang_jianhua@bankcomm.com>
2017-02-23 22:11:34 +0800

    Redirect after POST, and show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -12,8 +12,7 @@ class NewVisitorTest(unittest.TestCase):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            row_text, [row.text for row in rows])
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrive_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -41,7 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
         self.check_for_row_in_list_table('1:Buy peacock feathers')
 
         
@@ -50,7 +49,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
         
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1:Buy peacock feathers')
