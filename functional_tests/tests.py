f192c1b72976191a11900713b1f72bcaf5d83c32
zhang_jianhua <zhang_jianhua@bankcomm.com>
2017-02-25 09:59:22 +0800

    New test for mutiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,23 +1,36 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
-import unittest
-class NewVisitorTest(unittest.TestCase):
+
+MAX_WAIT = 10
+#import unittest
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrive_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        ## self.browser.get('http://localhost:8000') ## 要使用测试库，要通过live_server_url访问,测试环境使用8081端口
+        self.browser.get(self.live_server_url)
         
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do',self.browser.title)
@@ -40,8 +53,7 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
 
         
         # There is still a text box inviting her to add another item. She
@@ -49,22 +61,33 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(10)
         
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table('1:Buy peacock feathers')
-        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2:Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
         
-        
+        # Satisfied, she goes back to sleep
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:Buy peacock feathers')
+        
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
         self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
         
         # Satisfied, she goes back to sleep
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+#if __name__ == '__main__':
+#    unittest.main(warnings='ignore')
 
