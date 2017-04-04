0f1231fb45895feef31f7f6b7069eeb803e433e9
zhang_jianhua <zhang_jianhua@bankcomm.com>
2017-04-04 10:59:07 +0800

    Move FTs into their own individual files

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,72 +1,9 @@
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import WebDriverException
-import time
-import sys
-import os
 
-MAX_WAIT = 10
-#import unittest
-class NewVisitorTest(StaticLiveServerTestCase):
-    
-    #@classmethod
-    #def setUpClass(cls):
-    #    for arg in sys.argv:
-    #        if 'liveserver' in arg:
-    #            cls.server_url = 'http://' + arg.split('=')[1]
-    #            return
-    #    super().setUpClass()
-    #    cls.server_url = cls.live_server_url
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            setattr(self, 'live_server_url', 'http://'+ staging_server)
-
-    def tearDown(self):
-        self.browser.quit()
-
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.live_server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        #print(inputbox.location)
-        self.assertAlmostEqual(
-             inputbox.location['x'] + inputbox.size['width'] / 2,
-             512,
-             delta=10
-        )
-        # She starts a new list and the input box is nicely
-        # centered there too
-        inputbox.send_keys('testing')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1:testing')
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-             inputbox.location['x'] + inputbox.size['width'] / 2,
-             512,
-             delta=6
-        )
-
-    def wait_for_row_in_list_table(self, row_text):
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
+class NewVisitorTest(FunctionalTest):
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -154,13 +91,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-        
-        # Satisfied, she goes back to sleep
-
-#if __name__ == '__main__':
-#    unittest.main(warnings='ignore')
 
