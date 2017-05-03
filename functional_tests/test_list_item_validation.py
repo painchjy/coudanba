cc06f88a2b1bd60c9cd9c51c757125c321aaeed7
zhang_jianhua <zhang_jianhua@bankcomm.com>
2017-05-03 13:42:55 +0800

    New FT for clear the error message of duplication inputs

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,27 @@ from selenium.webdriver.common.keys import Keys
 from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a list and causes a validation error:
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Banter too thick')
+        self.get_item_input_box().send_keys('Banter too thick')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # She strarts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # She is pleased to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
+        ))
+        
     def test_cannot_add_duplicate_items(self):
         #Edith goes to the home page and starts a new list
         self.browser.get(self.live_server_url)
@@ -16,7 +37,7 @@ class ItemValidationTest(FunctionalTest):
 
         #She sees a helpfull error message
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this in your list"
         ))
 
@@ -64,7 +85,7 @@ class ItemValidationTest(FunctionalTest):
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You can't have an empty list item"
         ))
 
@@ -79,7 +100,7 @@ class ItemValidationTest(FunctionalTest):
 
         # She receives a similar warning on the list page
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You can't have an empty list item"
         ))
 
