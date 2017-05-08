6a4421ef388f9a8426aabb55284c0c865593c193
zhang_jianhua <zhang_jianhua@bankcomm.com>
2017-05-08 15:48:39 +0800

    update requirement

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -22,7 +22,7 @@ def _create_directory_structure_if_necessary(site_folder):
 
 def _get_latest_source(source_folder):
     if exists(source_folder + '/.git'):
-        run(f'cd {source_folder} && git pull')
+        run(f'cd {source_folder} && git fetch')
     else:
         run(f'git clone {REPO_URL} {source_folder}')
     current_commit = local("git log -n 1 --format=%H", capture=True)
