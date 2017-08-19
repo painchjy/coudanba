d8933ad248b13431bfb639a7cadf2a50553566e1
painchjy <painchjy@gmail.com>
2017-08-19 10:26:56 +0800

    model ju refactor add items in json

--- wechat/views.py
+++ wechat/views.py
@@ -6,24 +6,23 @@ import xmltodict
 from django.core.cache import caches
 from django.conf import settings
 from django.http import HttpResponse
-from wechatpy.replies import TextReply
-from wechatpy.utils import to_text
-
+from wechatpy.utils import check_signature
+from wechatpy.exceptions import InvalidSignatureException
 
 WECHAT_TOKEN = 'testwechatbyzjh'
 def check(request):
-    signature = request.args.get('signature', '')
-    timestamp = request.args.get('timestamp', '')
-    nonce = request.args.get('nonce', '')
-    echostr = request.args.get('echostr', '')
+    signature = request.GET.get('signature', '')
+    timestamp = request.GET.get('timestamp', '')
+    nonce = request.GET.get('nonce', '')
+    echostr = request.GET.get('echostr', '')
 
     try:
         check_signature(WECHAT_TOKEN, signature, timestamp, nonce)
     except InvalidSignatureException:
         # 处理异常情况或忽略
-        print('>>> {},{}'.format(request.args, '验证异常'))
+        print('>>> {},{}'.format(request.GET, '验证异常'))
         # return '验证异常'
         return 'Shutting down...'
     else:
-        print('>>> {},{}'.format(request.args, '验证ok'))
-        return echostr
+        print('>>> {},{}'.format(request.GET, '验证ok'))
+        return HttpResponse(echostr)
