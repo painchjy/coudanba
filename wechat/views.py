2e628417ae7aa73efc0dddea5b44c42e5b67c41b
painchjy <painchjy@gmail.com>
2017-08-19 16:59:16 +0800

    test wechat post

--- wechat/views.py
+++ wechat/views.py
@@ -12,21 +12,22 @@ from wechatpy.utils import check_signature
 from wechatpy.crypto import WeChatCrypto
 from wechatpy.exceptions import InvalidSignatureException, InvalidAppIdException
 from wechatpy.replies import TextReply
+from django.views.decorators.csrf import csrf_exempt
+
 WECHAT_TOKEN = os.environ.get('WECHAT_TOKEN')
 #AES_KEY = os.environ.get('AES_KEY')
 #APPID = os.environ.get('APPID')
+@csrf_exempt
 def interface(request):
     if request.method == 'GET':
         return get(request)
     else:
         return post(request)
 def post(request):
-    signature = request.POST.get('signature', '')
-    timestamp = request.POST.get('timestamp', '')
-    nonce = request.POST.get('nonce', '')
+    signature = request.GET.get('signature', '')
+    timestamp = request.GET.get('timestamp', '')
+    nonce = request.GET.get('nonce', '')
 
-    msg_signature = request.POST.get('msg_signature', '')
-    encrypt_type = request.POST.get('encrypt_type', '')
     try:
         check_signature(WECHAT_TOKEN, signature, timestamp, nonce)
     except InvalidSignatureException:
@@ -34,8 +35,11 @@ def post(request):
         #print('>>> {},{}'.format(request.args, '验证异常'))
         # return '验证异常'
         return 'Shutting down...'
+    msg_signature = request.POST.get('msg_signature', '')
+    encrypt_type = request.POST.get('encrypt_type', '')
     request_msg = parse_message(request.POST)
-    reply = TextReply(content='{}'.format(request_msg.content), message=request_msg)
+    print('>>> {},{}'.format(request.GET, request.POST))
+    reply = TextReply(content='text', message=request_msg)
     return reply.render()
 
 def get(request):
