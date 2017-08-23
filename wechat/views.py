bcd14ab11dfcfc75406909724e5a5d7afe61e887
painchjy <painchjy@gmail.com>
2017-08-23 23:21:38 +0800

    wechat app test menu event

--- wechat/views.py
+++ wechat/views.py
@@ -97,7 +97,9 @@ def response_message(xml, request=None):
         if msg.event == 'click' and msg.key == 'login':
             email = user.get('email')
             if email:
-                token = Token.objects.create(email=email)
+                token = Token.objets.filter(email=email).first()
+                if not token:
+                    token = Token.objects.create(email=email)
                 url = request.build_absolute_uri(
                     reverse('login') + '?token=' + str(token.uid)
                 )
