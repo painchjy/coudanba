b68fc4d50a4b95cd4f16e9e8b680950274a61404
painchjy <painchjy@gmail.com>
2017-08-30 18:02:18 +0800

    test oauth

--- wechat/views.py
+++ wechat/views.py
@@ -36,6 +36,7 @@ def oauth(method):
         client = WeChatClient(APPID, SECRET)
         code = request.GET.get('code', None)
         url = client.oauth.authorize_url(request.build_absolute_uri())
+        log.debug('>>>auth get url:{} request.uri:{}'.format(url, request.build_absolute_uri()))
         if not code:
             return method(request, *args, **kwargs)
         try:
@@ -64,8 +65,10 @@ def oauth(method):
             if not token:
                 token = Token.objects.create(email=userpk)
             user = auth.authenticate(uid=token)
+            log.debug('>>>auth user authenticated:{}'.format(user))
             if user:
                 auth.login(request, user)
+                log.debug('>>>auth user logged in:{}{}'.format(user,url))
                 return redirect(url)
 
         return method(request, *args, **kwargs)
